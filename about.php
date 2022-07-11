361a1c780294120dcd88f1df8de5f56c9b78727f
MadSkillsDev <you@example.com>
2022-07-11 10:57:53 +0300

    about page start

--- about.php
+++ about.php
@@ -41,7 +41,35 @@
         <h1>about us</h1>
     </div>
     <!-- about -->
-    
+    <section class="about">
+        <div class="image">
+            <img src="images/about-img.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>why choose us?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quia et suscipit maiores iusto. Quaerat
+                quisquam adipisci vitae repellendus enim.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quae sed animi, quidem rem sit omnis odio
+                dolore, atque, officiis ratione. Laudantium distinctio repudiandae id quas, tempore possimus similique.
+                Delectus.</p>
+                <div class="icons-container">
+                    <div class="icons">
+                        <i class="fas fa-map"></i>
+                        <span>top destinations</span>
+                    </div>
+                    <div class="icons">
+                        <i class="fas fa-hand-holding-usd"></i>
+                        <span>affordable price</span>
+                    </div>
+                    <div class="icons">
+                        <i class="fas fa-headset"></i>
+                        <span>24/7 guide service</span>
+                    </div>
+
+        </div>
+
+</section>
     <!-- end of about -->
 
 
